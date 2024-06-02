ca68cd97048b4df666460cf6fedc9c5fd9b96aa1
i_hate_n.... <twinceptacywer@gmail.com>
2024-06-02 10:16:47 +0300

    02.06

--- Assets/PlaerController.cs
+++ Assets/PlaerController.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class PlaerController : MonoBehaviour
 {
+  public float mouseSensitivity = 2f;
+  public GameObject camera;
+  private float mouse = 0;
+  private float pitch = 0;
   public float speed = 5f;
   private Rigidbody rb;
   private Vector3  direction;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -17,8 +22,10 @@ public class PlaerController : MonoBehaviour
     {
       Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
       rb.velocity=new(speed*targetVelocity.x,0f,speed*targetVelocity.z); 
-    float Mouse = transform.localEulerAngels.y + Input.GetAxis("Mouse X") * Mouse Sensitivity;
+      mouse = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * mouseSensitivity;
+      pitch -=  mouseSensitivity * Input.GetAxis("Mouse Y");
+      transform.localEulerAngles = new Vector3(0, mouse, 0);
+      camera.transform.localEulerAngles = new Vector3(pitch,0,0);
+      
     }
-   
-
 }
